ae79db9c37c760f101cf4c54755ba9863657f5df
lenharris <stacia.lt.harris@gmail.com>
2017-09-27 11:44:17 -0700

    Project 4

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var friends = ['Rob', 'Teryy', 'Marbury', 'La'];
+
+for (var i = 0; i<friends.length; i++) {
+    for (var j = 99; j>0; j--) {
+    console.log(j+ 'lines of code in the file,' + j+ ' lines of code;'+ ' '+ friends[i]+ ' strikes one out, clears it all out,  lines of code in the file');
+    }
+
+}
