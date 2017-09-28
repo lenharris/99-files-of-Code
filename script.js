100f7309cde2511e3fd6a74efa8f45a0d6e93ef5
lenharris <31999283+lenharris@users.noreply.github.com>
2017-09-28 07:29:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var friends = ['Rob', 'Teryy', 'Marbury', 'La'];
 
 for (var i = 0; i<friends.length; i++) {
     for (var j = 99; j>0; j--) {
-    console.log(j+ 'lines of code in the file,' + j+ ' lines of code;'+ ' '+ friends[i]+ ' strikes one out, clears it all out,  lines of code in the file');
+    console.log(j+ 'lines of code in the file,' + j+ ' lines of code;'+ ' '+ friends[i]+ ' strikes one out, clears it all out,' + (j-1) + ' lines of code in the file';
     }
 
 }
